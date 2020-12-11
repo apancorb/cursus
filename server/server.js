63b9fb78d92fb1cc5665c78f5f11afdb5d570906
apancorb <apancorb@terpmail.umd.edu>
2020-12-10 22:24:50 -0500

    first! :)

---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from "express";  
+import mongoose from "mongoose"; 
+import cors from "cors";
+import bodyParser from "body-parser";
+import "dotenv/config.js";
+
+import Cards from "./dbCards.js";
+
+/* APP CONFIG */
+const app = express();
+const port = process.env.PORT || 8003;
+const mongo_conection_url = undefined;
+
+/* MIDDLEWARES */
+app.use(bodyParser.json());
+app.use(cors())
+
+/* console.log(process.env.DB_CONNECTION); */
+/* DB CONFIG */
+/* mongoose.connect(mongo_conection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}); */
+
+/* API ENDPOINTS */
+app.get('/', (req, res) => res.status(200).send('It is working!'));
+
+/* app.post('/tinder/card', (req, res) => {
+    const dbCard = req.body;
+    console.log(dbCard)
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+            // 500 => internal status error
+        } else {
+            res.status(201).send(data);
+            // 201 => succesfully created since this is a post call
+        }
+    });
+}); */
+
+
+
+/* LISTENER */
+app.listen(port, () => console.log(`listening on localhost: ${port}`));

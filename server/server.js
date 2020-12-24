dcde706a4f6f1114f7969accdbeae23cade94817
apancorb <apancorb@terpmail.umd.edu>
2020-12-24 00:26:52 -0500

    some changes to server.js

--- server/server.js
+++ server/server.js
@@ -5,45 +5,19 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 dotenv.config();
 
-import Cards from "./dbCards.js";
-
 /* APP CONFIG */
 const app = express();
 const port = process.env.PORT || 8000;
 const mongo_conection_url = "";
 
 /* MIDDLEWARES */
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
-
-/* console.log(process.env.DB_CONNECTION); */
-/* DB CONFIG */
-/* mongoose.connect(mongo_conection_url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}); */
 
 /* API ENDPOINTS */
 app.get("/", (req, res) => res.status(200).send("It is working!"));
 
-/* app.post('/tinder/card', (req, res) => {
-    const dbCard = req.body;
-    console.log(dbCard)
-
-    Cards.create(dbCard, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send(err);
-            // 500 => internal status error
-        } else {
-            res.status(201).send(data);
-            // 201 => succesfully created since this is a post call
-        }
-    });
-}); */
-
-/* START WEB-SCRAPING INETRVAL */
+/* START WEB-SCRAPING INETERVAL */
 import "./web-scraping/interval.js";
 
 /* LISTENER */
